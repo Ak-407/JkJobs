f247a311f6976484c6ecf3f24b97ad9e88947aa0
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2024-11-29 20:43:08 +0530

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # jkstream
 # jkjobs
 # JkJobs
+# JkJobs
--- app.py
+++ app.py
@@ -1360,4 +1360,6 @@ def chat():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5900)))
+
+    # app.run(debug=True, port=8050, host='0.0.0.0')
